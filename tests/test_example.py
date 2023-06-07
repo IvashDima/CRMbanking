2aac81d747121f69765e86ec24d2cbe3998daa6b
demon <dnytsyk@gmail.com>
2023-06-07 21:39:50 +0200

    preparation pytest

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,11 @@
-from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
-
 import requests
+
+from jsonschema import validate
+
+from configuration import SERVICE_URL
+
+from src.enums.global_enums import GlobalErrorMessages
+from src.schemas.post import POST_SCHEMA
 
 
 def test_getting_posts():
@@ -10,6 +14,8 @@ def test_getting_posts():
     print(received_posts)
     assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
     assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+    for item in received_posts:
+        validate(item, POST_SCHEMA)
 
 
 # [{'id': 1, 'title': 'Post 1'}, {'id': 2, 'title': 'Post 2'}, {'id': 3, 'title': 'Post 3'}]
